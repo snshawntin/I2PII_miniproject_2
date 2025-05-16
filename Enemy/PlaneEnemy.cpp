70afffd300d0c22157eccad88bfb3920fe49d194
snshawntin <haoenxu1@gmail.com>
2025-05-16 13:40:30 +0800

    hackathon finished ver.

---
+++ Enemy/PlaneEnemy.cpp
@@ -0,0 +1,7 @@
+#include <string>
+
+#include "PlaneEnemy.hpp"
+
+//(END) TODO HACKATHON-3 (1/3): You can imitate the 2 files: 'SoldierEnemy.hpp', 'SoldierEnemy.cpp' to create a new enemy.
+PlaneEnemy::PlaneEnemy(int x, int y) : Enemy("play/enemy-2.png", x, y, 10, 50, 5, 5) {
+}
---
+++ main.cpp
@@ -0,0 +1,28 @@
+// [main.cpp]
+// This is the entry point of your game.
+// You can register your scenes here, and start the game.
+#include "Engine/GameEngine.hpp"
+#include "Engine/LOG.hpp"
+#include "Scene/LoseScene.hpp"
+#include "Scene/PlayScene.hpp"
+#include "Scene/StageSelectScene.hpp"
+#include "Scene/WinScene.hpp"
+#include "Scene/StartScene.h"
+#include "Scene/SettingsScene.hpp"
+
+int main(int argc, char **argv) {
+	Engine::LOG::SetConfig(true);
+	Engine::GameEngine& game = Engine::GameEngine::GetInstance();
+
+    //(END) TODO HACKATHON-2 (2/3): Register Scenes here
+	game.AddNewScene("start", new StartScene());
+    game.AddNewScene("stage-select", new StageSelectScene());
+	game.AddNewScene("settings", new SettingsScene());
+	game.AddNewScene("play", new PlayScene());
+	game.AddNewScene("lose", new LoseScene());
+	game.AddNewScene("win", new WinScene());
+
+    //(END) TODO HACKATHON-1 (1/1): Change the start scene
+	game.Start("start", 60, 1600, 832);
+	return 0;
+}
