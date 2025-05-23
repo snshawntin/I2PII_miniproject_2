2a6116b0eb19cdc30345d41c15e2eda7222d29d2
snshawntin <haoenxu1@gmail.com>
2025-05-24 02:12:53 +0800

    shield enemy first finished

---
+++ build/CMakeFiles/2025_I2P2_TowerDefense.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Bullet/Bullet.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/Bullet.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/Bullet.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Bullet/FireBullet.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/FireBullet.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/FireBullet.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Bullet/GrowBullet.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/Growbullet.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/Growbullet.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Bullet/LaserBullet.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/LaserBullet.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Bullet/LaserBullet.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Enemy/Enemy.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/Enemy.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/Enemy.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Enemy/PlaneEnemy.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/PlaneEnemy.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/PlaneEnemy.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Enemy/ShieldEnemy.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/ShieldEnemy.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/ShieldEnemy.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Enemy/SoldierEnemy.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/SoldierEnemy.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/SoldierEnemy.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Enemy/TankEnemy.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/TankEnemy.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Enemy/TankEnemy.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/Allegro5Exception.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Allegro5Exception.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Allegro5Exception.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/AudioHelper.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/AudioHelper.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/AudioHelper.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/Collider.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Collider.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Collider.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/GameEngine.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/GameEngine.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/GameEngine.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/Group.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Group.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Group.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/IControl.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/IControl.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/IControl.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/IObject.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/IObject.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/IObject.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/IScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/IScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/IScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/LOG.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/LOG.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/LOG.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/Point.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Point.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Point.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/Resources.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Resources.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Resources.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Engine/Sprite.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Sprite.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Engine/Sprite.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/LoseScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/LoseScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/LoseScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/PlayScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/PlayScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/PlayScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/ScoreboardScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/ScoreboardScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/ScoreboardScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/SettingsScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/SettingsScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/SettingsScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/StageSelectScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/StageSelectScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/StageSelectScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/StartScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/StartScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/StartScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Scene/WinScene.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/WinScene.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Scene/WinScene.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Turret/GrowTurret.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/GrowTurret.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/GrowTurret.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Turret/LaserTurret.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/LaserTurret.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/LaserTurret.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Turret/MachineGunTurret.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/MachineGunTurret.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/MachineGunTurret.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Turret/Turret.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/Turret.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/Turret.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/Turret/TurretButton.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/TurretButton.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/Turret/TurretButton.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Animation/DirtyEffect.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Animation/DirtyEffect.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Animation/DirtyEffect.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Animation/ExplosionEffect.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Animation/ExplosionEffect.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Animation/ExplosionEffect.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Animation/Plane.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Animation/Plane.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Animation/Plane.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Component/Image.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/Image.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/Image.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Component/ImageButton.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/ImageButton.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/ImageButton.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Component/Label.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/Label.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/Label.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/UI/Component/Slider.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/Slider.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/UI/Component/Slider.cpp.obj.d"
+  "C:/Users/haoen/Desktop/2025_I2P2_TowerDefense-main/main.cpp" "CMakeFiles/2025_I2P2_TowerDefense.dir/main.cpp.obj" "gcc" "CMakeFiles/2025_I2P2_TowerDefense.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
